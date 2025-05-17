169f34b800ce13f48dbfb6a44e790a765ed626ae
DimaGusakov <dimagusakov2005@gmail.com>
2025-05-17 23:38:57 +0300

    project-template2

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App/App'
-import { Provider } from 'redux'
+import { Provider } from 'react-redux'
 import { store } from './store/store'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <ChartContainer>
+        <BarChart data={data}>
+          <Bar dataKey="value" />
+          <ChartTooltip content={<ChartTooltipContent />} />
+        </BarChart>
+      </ChartContainer>
     </Provider>
   </StrictMode>,
 )
