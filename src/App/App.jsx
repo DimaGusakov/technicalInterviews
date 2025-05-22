407914ad1c0ce02617bba72095ba3a24f5e3af5b
EveLine15 <nnasta788@gmail.com>
2025-05-22 12:06:20 +0300

    clear platform_2 branch

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import './App.scss';
 
-import ProfileNavigation from '@/pages/ProfileNavigation/ProfileNavigation';
 import Profile from '@/pages/Profile/Profile';
 import EditingProfile from '@/components/EditingProfile/EditingProfile';
 
@@ -10,7 +9,7 @@ export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/profileNavigation" element={<ProfileNavigation/>}>
+                <Route path="/profileNavigation" >
                     <Route path="profile" element={<Profile/>} />
                     <Route path='edit' element={<EditingProfile/>} />
                 </Route>                
