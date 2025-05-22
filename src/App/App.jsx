d0c264d7a9b7910393fe4fd5d7b47356fb12b3fc
EveLine15 <nnasta788@gmail.com>
2025-05-22 11:31:51 +0300

    clean platform_1: keep only ProfileNavigation

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,16 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import './App.scss';
 
 import ProfileNavigation from '@/pages/ProfileNavigation/ProfileNavigation';
-import Profile from '@/pages/Profile/Profile';
-import EditingProfile from '@/components/EditingProfile/EditingProfile';
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/profileNavigation" element={<ProfileNavigation/>}>
-                    <Route path="profile" element={<Profile/>} />
-                    <Route path='edit' element={<EditingProfile/>} />
+                    <Route path="profile"/>
+                    <Route path='edit'/>
                 </Route>                
             </Routes>
         </BrowserRouter>
