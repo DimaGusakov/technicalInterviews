db7011194c0a05ff1dd509d90c4f6bded503caaa
EveLine15 <nnasta788@gmail.com>
2025-05-27 19:24:53 +0300

    commit

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import './App.scss';
 
+import Education from '@/pages/Education/Education';
+
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-             
+                <Route path="/education" element={<Education/>}/>
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/Education/Education.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Education() {
+  return (
+    <div className="p-4 border rounded-3xl shadow-[0px_4px_10px_0px_rgba(106,99,118,0.1)] max-h-73">
+      <h1 className="text-xl font-semibold mb-2">dwdwd</h1>
+
+    </div>
+  )
+}
