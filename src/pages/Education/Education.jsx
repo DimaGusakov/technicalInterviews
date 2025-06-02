8795a4245c2a2af8a10143e494a3af9f9a10b36d
EveLine15 <nnasta788@gmail.com>
2025-06-02 09:24:52 +0300

    base style

--- src/pages/Education/Education.jsx
+++ src/pages/Education/Education.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Education() {
   return (
     <div className="p-4 border rounded-3xl shadow-[0px_4px_10px_0px_rgba(106,99,118,0.1)] max-h-73">
-      <h1 className="text-xl font-semibold mb-2">dwdwd</h1>
+      <h1 className="text-xl font-semibold mb-2">Подготовка к собеседываниям</h1>
 
     </div>
   )
