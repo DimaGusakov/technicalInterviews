761fdec1eb12dcc57bbe69ab046fb0bb88837d01
EveLine15 <nnasta788@gmail.com>
2025-06-08 02:10:01 +0300

    готовая страница с вопросами

--- src/pages/TestBlock/TestBlock.jsx
+++ src/pages/TestBlock/TestBlock.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useState, useEffect, useRef } from "react";
 import { generateQuetions, generateFeedback } from './AiGeneration/generator';
+import { Button } from '@/components/ui/button';
 
 export default function TestBlock(props) {
   const [quetions, setQuetions] = useState([]);  // array of q
   const [testUserInfo, setTestUserInfo] = useState({ // user data
-    numberOfQuetions: 1,
+    numberOfQuetions: 2,
     profession: "Фронтенд-разработчик",
     level: "Junior"
   })
@@ -32,10 +33,13 @@ export default function TestBlock(props) {
   const nextQuetion = async () => {
     try {
       const feedback = await generateFeedback(answer);
-      setResults(prev => prev.push(feedback));
+      setResults(prev => {
+        const updated = [...prev, feedback];
+        console.log(updated); // This will show the correct array
+        return updated;
+      });
 
       if (testUserInfo.numberOfQuetions - 1 !== currentQuetion) setCurrentQuetion(prev =>  prev + 1);
-      else console.log(results);
 
     } catch (error) {
       console.error(error)
@@ -44,11 +48,19 @@ export default function TestBlock(props) {
   }
 
   return (
-    <div className='wr-test'>
+    <div className='p-[16px] m-10 bg-[var(--color-card)] border border-[var(--color-border)] rounded-3xl shadow-[0px_4px_10px_0px_rgba(106,99,118,0.1)]'>
         <div>
-            <h1>{quetions[currentQuetion]}</h1>
-            <textarea name="answer" id="answer" value={answer} onChange={(e) => setAnswer(e.target.value)}></textarea>
-            <button onClick={nextQuetion}>Next</button>
+            <p className='my-2 text-[#757575] text-sm font-medium leading-[120%]'>Вопрос {currentQuetion + 1} из {testUserInfo.numberOfQuetions}</p>
+            <h1 className='text-black font-manrope text-[18px] font-semibold leading-[120%]'>{quetions[currentQuetion]}</h1>
+        </div>
+        <div className='my-4'>
+            <textarea className='mb-2 w-full h-40 p-4 border border-gray-300 rounded-2xl bg-white text-gray-800 text-base font-medium focus:outline-none focus:ring-2 focus:ring-purple-400 resize-none overflow-auto scrollbar-hide placeholder-gray-400' 
+                      name="answer" 
+                      id="answer" 
+                      value={answer} 
+                      placeholder='Напишите свой развёрнутый ответ'
+                      onChange={(e) => setAnswer(e.target.value)}></textarea>
+            <Button onClick={nextQuetion}>{testUserInfo.numberOfQuetions - 1 !== currentQuetion ? "Далее" : "Завершить"}</Button>
         </div>
     </div>
   )
